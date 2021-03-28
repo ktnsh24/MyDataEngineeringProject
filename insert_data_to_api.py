fc1ec21882689a0d3d9ad597d9396e6863c7fabc
Ketan Sahu <36641367+ktnsh24@users.noreply.github.com>
2021-03-28 14:08:04 +0200

    firstCommit

---
+++ insert_data_to_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import requests
+
+
+# write all the rows from users_app_data_stream to the api as PUT request
+def sample_function():
+
+    myURL = "https://zp91xerpvb.execute-api.us-east-1.amazonaws.com/prod_temp/my-first-api"
+
+    # Read the testfile
+    data = pd.read_csv(
+        '/Users/ketansahu/Documents/DataEngineeringProject/GitHubRepo/users_app_data_stream.csv', sep=',')
+    for i in data.index:
+        #print(i)
+        try:
+            # convert the row to json
+            export = data.loc[i].to_json()
+
+            # send it to the api
+            response = requests.post(myURL, data=export)
+
+            # print the returncode
+            print(export)
+            print(response)
+        except:
+            print(data.loc[i])
