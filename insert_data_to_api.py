a4ec58aa9db70c7247ff4c8537811e3c826f36ba
Ketan Sahu <ktnsh24@gmail.com>
2021-03-30 07:44:01 +0200

    Images

--- insert_data_to_api.py
+++ insert_data_to_api.py
@@ -4,10 +4,12 @@
 
 import pandas as pd
 import requests
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
 
 
 # write all the rows from users_app_data_stream to the api as PUT request
-def sample_function():
+def sample_function(sc):
 
     myURL = "https://zp91xerpvb.execute-api.us-east-1.amazonaws.com/prod_temp/my-first-api"
 
@@ -28,3 +30,10 @@ def sample_function():
             print(response)
         except:
             print(data.loc[i])
+    s.enter(60, 1, sample_function, (sc,))
+    
+    
+s.enter(60, 1, sample_function, (s,))
+print("data is send")
+s.run()
+
